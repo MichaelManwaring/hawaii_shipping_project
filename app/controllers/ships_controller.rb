293f4cdaeb27d7366b4fc5e6009cb0615bebc918
Erin Mahon <erin.mahon14@gmail.com>
2016-01-15 11:32:31 -0500

    Initial functionality for creating a new ship

--- app/controllers/ships_controller.rb
+++ app/controllers/ships_controller.rb
@@ -1,15 +1,37 @@
 class ShipsController < ApplicationController
-  before_filter :authorize
+  #before_filter :authorize
 
   def index
+  	@ships = Ship.all
+  end
+
+  def create
+  	@ship = Ship.create(ship_params)
+  	redirect_to root_path
   end
 
   def new
+  	@ship = Ship.new
   end
 
   def show
+  	@ship = Ship.find(params[:id])
+  end
+
+  def update
   end
 
   def edit
+  end
+
+  def destroy
+  end
+
+  private
+
+  # this provdes additional security by only allowing the permitted variables to be accessed and changed.
+
+  def ship_params
+    params.require(:ship).permit(:ship_name, :container_space, :current_location)
   end
 end
