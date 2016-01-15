5669df44af52299fa091b15ccb4e91d8d0fa7906
Michael Manwaring <Michael.c.Manwaring@gmail.com>
2016-01-15 16:36:05 -0500

    jobs can be created and attach to current user

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160115160833) do
+ActiveRecord::Schema.define(version: 20160115212959) do
 
   create_table "jobs", force: :cascade do |t|
     t.string   "job_name"
@@ -22,6 +22,9 @@ ActiveRecord::Schema.define(version: 20160115160833) do
     t.datetime "created_at",        null: false
     t.datetime "updated_at",        null: false
     t.integer  "trip_id"
+    t.integer  "origin"
+    t.integer  "destination"
+    t.integer  "user_id"
   end
 
   create_table "routes", force: :cascade do |t|
