baa8a8e92c6fb70540024cb26dd7024296821363
Michael Manwaring <Michael.c.Manwaring@gmail.com>
2016-01-18 01:18:27 -0500

    Website Fully Functional

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -1,12 +1,20 @@
 class TripsController < ApplicationController
   before_filter :authorize
+  def create
+  	
+  end
+
   def index
   end
 
   def new
+
   end
 
   def show
+  	@trip=Trip.find(params[:id])
+  	@trip.status=2
+  	
   end
 
   def edit
